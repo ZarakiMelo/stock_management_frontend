f11a12de4c8c6d06f549a99265713d0d1b557ea7
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-20 16:44:52 +0200

    Implement ItemList component

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+// src/app/components/item-list/item-list.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../services/item.service';
+import { Item } from '../../models/item.model';
 
 @Component({
   selector: 'app-item-list',
-  standalone: true,
-  imports: [],
   templateUrl: './item-list.component.html',
-  styleUrl: './item-list.component.css'
+  styleUrls: ['./item-list.component.css']
 })
-export class ItemListComponent {
+export class ItemListComponent implements OnInit {
+  items: Item[] = [];
 
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    this.itemService.getItems().subscribe(items => this.items = items);
+  }
+
+  deleteItem(id: string): void {
+    this.itemService.deleteItem(id).subscribe(() => {
+      this.items = this.items.filter(item => item._id !== id);
+    });
+  }
 }
